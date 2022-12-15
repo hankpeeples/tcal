46656d608b1d0b8e2d4f18fb0563fb3fb121d001
Henry Peeples <hgpeeples48@gmail.com>
2022-12-15 05:17:19 -0600

    Add table formatted printer

---
+++ table.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"github.com/charmbracelet/bubbles/table"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+var baseStyle = lipgloss.NewStyle().
+	BorderStyle(lipgloss.NormalBorder()).
+	BorderForeground(lipgloss.Color("#c0a46b"))
+
+type model struct {
+	table table.Model
+}
+
+func (m model) Init() tea.Cmd { return nil }
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "esc":
+			if m.table.Focused() {
+				m.table.Blur()
+			} else {
+				m.table.Focus()
+			}
+		case "q", "ctrl+c":
+			return m, tea.Quit
+		case "enter":
+			return m, tea.Batch(
+				tea.Printf("Let's go to %s!", m.table.SelectedRow()[1]),
+			)
+		}
+	}
+	m.table, cmd = m.table.Update(msg)
+	return m, cmd
+}
+
+func (m model) View() string {
+	return baseStyle.Render(m.table.View()) + "\n"
+}
+
+func printEventList(list []calEvent) {
+	columns := []table.Column{
+		{Title: "Event Name", Width: 25},
+		{Title: "Date", Width: 15},
+		{Title: "Description", Width: 15},
+		{Title: "Type", Width: 8},
+		{Title: "Status", Width: 10},
+		{Title: "Updated", Width: 15},
+	}
+
+	var rows []table.Row
+	for _, e := range list {
+		rows = append(rows, table.Row{e.Name, e.Date, e.Description, e.Type, e.Status, e.Updated})
+	}
+
+	t := table.New(
+		table.WithColumns(columns),
+		table.WithRows(rows),
+		table.WithFocused(true),
+	)
+
+	tea.NewProgram(model{t}).Run()
+}
