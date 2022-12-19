6a028c6160d90dc091459a9e5c83026c5a5a642b
Henry Peeples <hgpeeples48@gmail.com>
2022-12-18 22:30:45 -0600

    Refactor to utils functions

--- main.go
+++ main.go
@@ -1,16 +1,9 @@
 package main
 
 import (
-	"errors"
-	"log"
-	"os"
-
 	garg "github.com/alexflint/go-arg"
 	"github.com/gookit/slog"
 	"github.com/gookit/slog/handler"
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/google"
-	"google.golang.org/api/calendar/v3"
 )
 
 // Log is a logger that writes to the tcal log file
@@ -33,31 +26,12 @@ func main() {
 
 	garg.MustParse(&Options)
 
-	// if token file doesn't exist, get auth and create it
-	if _, err := os.Stat("./token.json"); errors.Is(err, os.ErrNotExist) {
+	if NeedsLogin() {
 		GetClient(ReadJSONConfig())
 	}
-	// When the user is initialy authenticated, the client needs to be re-configured
-	// or the refresh token will be invalid
 
 	// reconnect auth with token file config
 	client := GetClient(ReadJSONConfig())
 
 	GetCalendar(client)
-}
-
-// ReadJSONConfig reads the json file holding app client secret values
-func ReadJSONConfig() *oauth2.Config {
-	b, err := os.ReadFile("client_secret.json")
-	if err != nil {
-		log.Printf("Can't read file... %s", err)
-	}
-
-	// If modifying these scopes, delete your previously saved token.json.
-	config, err := google.ConfigFromJSON(b, calendar.CalendarReadonlyScope)
-	if err != nil {
-		log.Printf("Unable to parse client secret file to config: %v", err)
-	}
-
-	return config
 }
